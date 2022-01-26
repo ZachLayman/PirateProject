96c210eb8fa501dc6bd3a308ca4550f7a22f8cd2
duyhoang9 <97994456+duyhoang9@users.noreply.github.com>
2022-01-26 13:06:46 -0500

    Add files via upload

---
+++ Pirate Pillagers/src/pirate/pillagers/Character.java
@@ -0,0 +1,17 @@
+package pirate.pillagers;
+
+public class Character {
+    int x;
+    int y;
+    int speed;
+    
+    public Character () {
+        
+    }
+    
+    public Character (int x, int y, int speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+}
---
+++ Pirate Pillagers/src/pirate/pillagers/Player.java
@@ -0,0 +1,13 @@
+package pirate.pillagers;
+
+public class Player extends Character {
+    
+    boolean moveRight;
+    boolean moveLeft;
+    
+    public Player (int x, int y, int s) {
+        super (x,y,s);
+        moveLeft = false;
+        moveRight = false;
+    }
+}
